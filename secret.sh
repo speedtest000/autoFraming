5522b16e7843756cb273ed13eadd2c1fa1e68533
speedtest000 <144241428+speedtest000@users.noreply.github.com>
2023-09-06 20:10:18 +0700

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -1,2 +1,1 @@
-fb_api_key=""
-giphy_api_key=""
+fb_api_key="EAAXFPkV9u4oBO6gME83RQyKsF2ce5nncGq79kCEiLFZBF27DLotazFgbGLDEXl8VNtSlhKcQS0Y6QwYUfsYxSjepxGjC6AZBIhqtqS8JbYMjcZCBZAuUhgPCYauluA4jILHlz74Jo1Gq3oVNsa8cNuYrhOUvtksir4SgTja7JRGbdEfp9Yia8yA0"
